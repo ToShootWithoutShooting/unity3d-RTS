f3e38550f9af0458da2fb8f7ea4ca9f2049fabe9
ToShootWithoutShooting <18242095960@163.com>
2019-10-29 13:19:01 +0800

    no message

---
+++ Assets/Pool/DelayDestory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayDestory : MonoBehaviour {
+
+    public float delayTime;
+
+    private void OnEnable()
+    {
+        Invoke("DelayDisable", delayTime);
+    }
+
+    private void DelayDisable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+}
